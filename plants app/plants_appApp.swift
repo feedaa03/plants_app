ac426bfb52aea192d5759ec262041c519e0399b6
Feda <feeeda2003@gmsil.com>
2025-10-19 09:35:36 +0300

    Initial Commit

---
+++ plants app/plants_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  plants_appApp.swift
+//  plants app
+//
+//  Created by Feda  on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct plants_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ plants appTests/plants_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  plants_appTests.swift
+//  plants appTests
+//
+//  Created by Feda  on 19/10/2025.
+//
+
+import Testing
+@testable import plants_app
+
+struct plants_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
